6db0f1e51f9e619263531ca7de84921b759ea478
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-04-07 16:34:32 +0200

    getUser_getCourt

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/db/Database.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/db/Database.java
@@ -1,9 +1,14 @@
 package pti.sb_squash_mvc.db;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import pti.sb_squash_mvc.model.Court;
+import pti.sb_squash_mvc.model.User;
 
 public class Database {
 	
@@ -14,8 +19,31 @@ public class Database {
 		sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 	}
 	
+	public User getUserById(int id) {
+		User user = null;
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		user = session.get(User.class, id);
+				
+		tx.commit();
+		session.close();
+				
+		return user;
+	}
 	
-	
+	public Court getCourtById(int id) {
+		Court court = null;
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		court = session.get(Court.class, id);
+				
+		tx.commit();
+		session.close();
+				
+		return court;
+	}
 	
 	
 	
