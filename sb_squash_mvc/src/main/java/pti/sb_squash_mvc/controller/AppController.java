a02d51c52b1b3b063f383c618090f890255262cd
KatalinDorman <dkata75@gmail.com>
2023-04-07 09:22:08 +0200

    Creating login page

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
@@ -1,6 +1,16 @@
 package pti.sb_squash_mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AppController {
 
+    
+    @GetMapping("/")
+    public String loadLoginPage(){
+        
+        return "login.html";
+    }
     
 }
