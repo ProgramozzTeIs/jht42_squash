264469422b33b8e77717c8137f61cf859b2b816e
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-04-07 15:56:06 +0200

    Update Court.java

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/model/Court.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/model/Court.java
@@ -1,5 +1,66 @@
 package pti.sb_squash_mvc.model;
 
-public class Court {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "court")
+public class Court {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "rent")
+	private int rent;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getRent() {
+		return rent;
+	}
+
+	public void setRent(int rent) {
+		this.rent = rent;
+	}
+
+	@Override
+	public String toString() {
+		return "Court [id=" + id + ", name=" + name + ", address=" + address + ", rent=" + rent + "]";
+	}
+	
+	
+	
 }
